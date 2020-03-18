1713a214afa4f20dfe30155d3c643b67fe2d4de1
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-03-18 17:31:29 +0900

    create_edit_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, only: [:index, :new, :create, :show, :edit]
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: [:edit, :update]
 end
