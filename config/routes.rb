e837eb83bcb2d5b1679baf02c573afd79b1abf35
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-03-30 11:15:03 +0900

    add_group_function

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   resources :messages, only: [:index]
   resources :users, only: [:edit, :update, :show]
   resources :cards, only: [:new, :create, :edit, :update, :show]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
